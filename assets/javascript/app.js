d7d4343d6aa7976db6e979347b208106c14b2d00
Michelle Castellanos <Michelle@Michelles-MacBook-Pro.local>
2016-11-24 16:55:26 -0600

    second commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,10 +2,7 @@
     var secs = mins * 60;
     var currentSeconds = 0;
     var currentMinutes = 0;
-    /* 
-     * The following line has been commented out due to a suggestion left in the comments. The line below it has not been tested. 
-     * setTimeout('Decrement()',1000);
-     */
+    
     setTimeout(Decrement,1000); 
 
     function Decrement() {
@@ -15,9 +12,8 @@
         secs--;
         document.getElementById("timerText").innerHTML = currentMinutes + ":" + currentSeconds; //Set the element id you need the time put into.
         if(secs !== -1) setTimeout('Decrement()',1000);
-        if(secs === 0) alert("YOU RAN OUT OF TIME");
-       
-    }
+        if(secs === 0) alert("Time is up! Click submit to see score");
+       }
 
 function onSubmit(){
 	var score = 0;
